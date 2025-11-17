fab56974012ea2872ef62a30667c8fa367040d4a
klfischer89 <fischer-k4@web.de>
2025-11-17 15:50:17 +0100

    second iteration , getting content of one email

--- spam_filter_iteration_I.py
+++ spam_filter_iteration_I.py
@@ -8,3 +8,13 @@ os.path.exists("./data/Spam-Emails.zip") # check if file exists
 if not os.path.exists("./data/Spam-Emails"): # extract spam mails if not allready done
     with zipfile.ZipFile("./data/Spam-Emails.zip", "r") as z: # open zipfile as z
         z.extractall("./data/Spam-Emails") 
+
+path = "./data/Spam-Emails/spam/00001.7848dde101aa985090474a91ec93fcf0" # get path to first email
+
+with open(path, mode = "r", encoding = "utf-8") as file: # open mail and truncate header
+    email = file.read()
+    content = email.split("\n\n", 1)[1]
+
+
+    
+    
